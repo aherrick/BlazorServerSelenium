170ff5ae51c2b7222504b337d2cfdd7a6d7b1a76
Andrew Herrick <aj@andrewherrick.com>
2020-04-16 14:26:13 -0400

    Toast

--- BlazorServerSelenium/Startup.cs
+++ BlazorServerSelenium/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Blazored.Toast;
+using BlazorServerSelenium.Data;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using BlazorServerSelenium.Data;
 
 namespace BlazorServerSelenium
 {
@@ -29,6 +24,8 @@ namespace BlazorServerSelenium
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+
+            services.AddBlazoredToast();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
